2dda83f22db2ee1b4ad7277b24e33886e5a10e70
rsz <rhenansz@hotmail.com>
2022-07-17 18:55:10 -0300

    feat: created investimentsRoutes

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 // import login from './controllers/login';
-// import investimentsRoutes from './routes/investimentsRoutes';
+import investimentsRoutes from './routes/investimentsRoutes';
 // import assetsRoutes from './routes/assetsRoutes';
 // import accountsRoutes from './routes/accountsRoutes';
 
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 // app.post('/login', login);
-// app.use('/investiments', investimentsRoutes);
+app.use('/investiments', investimentsRoutes);
 // app.use('/assets', assetsRoutes);
 // app.use('/accounts', accountsRoutes);
 
---
+++ routes/investimentsRoutes.js
@@ -0,0 +1,5 @@
+import express from 'express';
+
+const investimentsRoutes = express.Router();
+
+export default investimentsRoutes;
