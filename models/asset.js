ef51ec3919b9a474971fbcd2799b5b1c83ddbc20
rsz <rhenansz@hotmail.com>
2022-07-17 15:53:13 -0300

    feat: created the Asset Model

---
+++ models/asset.js
@@ -0,0 +1,18 @@
+export default (sequelize, DataTypes) => {
+  const Asset = sequelize.define('Asset', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    quantity: DataTypes.INTEGER,
+    price: DataTypes.DECIMAL(65, 2)
+  },
+  {
+    timestamps: false,
+    underscored: true,
+    tableName: 'Assets'
+  });
+
+  return Asset;
+};
