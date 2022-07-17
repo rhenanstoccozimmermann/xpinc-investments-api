87a10e6e17b139f69f656fc89cb07c272b519cda
rsz <rhenansz@hotmail.com>
2022-07-17 18:23:38 -0300

    feat: created api.js

---
+++ api.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+// import login from './controllers/login';
+// import investimentsRoutes from './routes/investimentsRoutes';
+// import assetsRoutes from './routes/assetsRoutes';
+// import accountsRoutes from './routes/accountsRoutes';
+
+const app = express();
+
+app.use(express.json());
+
+// app.post('/login', login);
+// app.use('/investiments', investimentsRoutes);
+// app.use('/assets', assetsRoutes);
+// app.use('/accounts', accountsRoutes);
+
+export default app;
