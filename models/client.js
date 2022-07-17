723a0507c762e353ba560790f72d60a081660f23
rsz <rhenansz@hotmail.com>
2022-07-17 15:47:16 -0300

    feat: created the Client Model

---
+++ models/client.js
@@ -0,0 +1,27 @@
+export default (sequelize, DataTypes) => {
+  const Client = sequelize.define('Client', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: DataTypes.STRING,
+    password: DataTypes.STRING,
+    accountId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true
+    }
+  },
+  {
+    timestamps: false,
+    underscored: true,
+    tableName: 'Clients'
+  });
+
+  Client.associate = (models) => {
+    Client.belongsTo(models.Account,
+      { foreignKey: 'accountId', as: 'accounts' });
+  };
+
+  return Client;
+};
