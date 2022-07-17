d4101cfc7034e5c55b3c0f81074a840f85fc6432
rsz <rhenansz@hotmail.com>
2022-07-17 15:51:12 -0300

    feat: created the Account Model

---
+++ models/account.js
@@ -0,0 +1,22 @@
+export default (sequelize, DataTypes) => {
+  const Account = sequelize.define('Account', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    balance: DataTypes.DECIMAL(65, 2)
+  },
+  {
+    timestamps: false,
+    underscored: true,
+    tableName: 'Accounts'
+  });
+
+  Account.associate = (models) => {
+    Account.hasOne(models.Client,
+      { foreignKey: 'id', as: 'clients' });
+  };
+
+  return Account;
+};
