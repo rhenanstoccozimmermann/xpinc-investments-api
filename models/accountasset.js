52acb77b64f9ce7e63fc78f9337418909a56c64d
rsz <rhenansz@hotmail.com>
2022-07-17 15:55:14 -0300

    feat: created the AccountAsset Model

---
+++ models/accountasset.js
@@ -0,0 +1,35 @@
+export default (sequelize, DataTypes) => {
+  const AccountAsset = sequelize.define('AccountAsset', {
+    accountId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    assetId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    quantity: DataTypes.INTEGER
+  },
+  {
+    timestamps: false,
+    underscored: true,
+    tableName: 'AccountAssets'
+  });
+
+  AccountAsset.associate = (models) => {
+    models.Account.belongsToMany(models.Asset, {
+      as: 'assets',
+      through: AccountAsset,
+      foreignKey: 'accountId',
+      otherKey: 'assetId'
+    });
+    models.Asset.belongsToMany(models.Account, {
+      as: 'accounts',
+      through: AccountAsset,
+      foreignKey: 'assetId',
+      otherKey: 'accountId'
+    });
+  };
+
+  return AccountAsset;
+};
