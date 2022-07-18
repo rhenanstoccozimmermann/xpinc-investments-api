b7f50cc7ff5ab0eb00adb18be2bea0dc61f7f93b
rsz <rhenansz@hotmail.com>
2022-07-18 09:13:57 -0300

    refactor: changed the endpoint POST /investments/sell to PUT /investments/sell

--- routes/investmentsRoutes.js
+++ routes/investmentsRoutes.js
@@ -6,6 +6,6 @@ import sellAsset from '../controllers/sellAsset';
 const investmentsRoutes = express.Router();
 
 investmentsRoutes.post('/buy', buyAsset);
-investmentsRoutes.post('/sell', sellAsset);
+investmentsRoutes.put('/sell', sellAsset);
 
 export default investmentsRoutes;
