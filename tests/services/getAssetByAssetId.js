c48dfe02746164898036897582c069bebee047fd
rsz <rhenansz@hotmail.com>
2022-07-23 23:09:18 -0300

    feat: added required fields validation on getAssetByAssetId service test

--- tests/services/getAssetByAssetId.js
+++ tests/services/getAssetByAssetId.js
@@ -5,6 +5,34 @@ const getAssetByAssetIdService = require('../../services/getAssetByAssetId');
 const { Asset } = require('../../models');
 
 describe('Ao chamar o service getAssetByAssetId', () => {
+  describe('quando as informações obrigatórias não são passadas', async () => {
+    const assetId = undefined;
+    
+    const code = 400;
+    const message = 'O código do ativo é obrigatório.';    
+
+    it('retorna um objeto de erro', async () => {
+      const response = await getAssetByAssetIdService.getAssetByAssetId(assetId);
+
+      expect(response).to.be.a('object');
+    });
+
+    it('tal objeto possui as propriedades de erro', async () => {
+      const response = await getAssetByAssetIdService.getAssetByAssetId(assetId);
+
+      expect(response).to.have.a.property('error');
+      expect(response.error).to.have.a.property('code');
+      expect(response.error).to.have.a.property('message');
+    });
+
+    it('tais propriedades possuem os valores corretos', async () => {
+      const response = await getAssetByAssetIdService.getAssetByAssetId(assetId);
+  
+      expect(response.error.code).to.be.equal(code);
+      expect(response.error.message).to.be.equal(message);
+    });
+  });
+
   describe('quando o ativo informado não é encontrado', async () => {
     const assetId = 20;
     
