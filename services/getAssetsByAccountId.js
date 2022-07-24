d633d545c2457db531356d62edb81fd481ea90a4
rsz <rhenansz@hotmail.com>
2022-07-23 22:44:41 -0300

    feat: added required fields validation on getAssetsByAccountId and getBalanceFromAccount service

--- services/getAssetsByAccountId.js
+++ services/getAssetsByAccountId.js
@@ -1,6 +1,23 @@
 const { Account, Asset } = require('../models');
 
+const validateData = (accountId) => {
+  if (accountId === undefined) {
+    return {
+      error: {
+        code: 400,
+        message: 'O código da conta é obrigatório.',
+      },
+    };
+  }
+
+  return {};
+};
+
 const getAssetsByAccountId = async (accountId) => {
+  const dataValidation = validateData(accountId);
+
+  if (dataValidation.error) return dataValidation;
+
   const accountAsset = await Account.findOne({
     where: { id: accountId },
     include: [
--- services/getBalanceFromAccount.js
+++ services/getBalanceFromAccount.js
@@ -1,6 +1,23 @@
 const { Account } = require('../models');
 
+const validateData = (accountId) => {
+  if (accountId === undefined) {
+    return {
+      error: {
+        code: 400,
+        message: 'O código da conta é obrigatório.',
+      },
+    };
+  }
+
+  return {};
+};
+
 const getBalanceFromAccount = async (accountId) => {
+  const dataValidation = validateData(accountId);
+
+  if (dataValidation.error) return dataValidation;
+
   const account = await Account.findByPk(accountId);
 
   if (!account) {
