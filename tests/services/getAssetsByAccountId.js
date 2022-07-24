cfd53141f2ec8bb1f1acc770b6d9fe4e0f65c4c2
rsz <rhenansz@hotmail.com>
2022-07-23 23:11:39 -0300

    feat: added required fields validation on getAssetsByAccountId service test

--- tests/services/getAssetsByAccountId.js
+++ tests/services/getAssetsByAccountId.js
@@ -5,6 +5,34 @@ const getAssetsByAccountIdService = require('../../services/getAssetsByAccountId
 const { Account, Asset } = require('../../models');
 
 describe('Ao chamar o service getAssetsByAccountId', () => {
+  describe('quando as informações obrigatórias não são passadas', async () => {
+    const accountId = undefined;
+    
+    const code = 400;
+    const message = 'O código da conta é obrigatório.';    
+
+    it('retorna um objeto de erro', async () => {
+      const response = await getAssetsByAccountIdService.getAssetsByAccountId(accountId);
+
+      expect(response).to.be.a('object');
+    });
+
+    it('tal objeto possui as propriedades de erro', async () => {
+      const response = await getAssetsByAccountIdService.getAssetsByAccountId(accountId);
+
+      expect(response).to.have.a.property('error');
+      expect(response.error).to.have.a.property('code');
+      expect(response.error).to.have.a.property('message');
+    });
+
+    it('tais propriedades possuem os valores corretos', async () => {
+      const response = await getAssetsByAccountIdService.getAssetsByAccountId(accountId);
+  
+      expect(response.error.code).to.be.equal(code);
+      expect(response.error.message).to.be.equal(message);
+    });
+  });
+
   describe('quando não são encontrados ativos na carteira', async () => {
     const accountId = 1;
     
