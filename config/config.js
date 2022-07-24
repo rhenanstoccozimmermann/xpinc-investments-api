ce95266ba053f94fde5d34d1cbdada68e375d69e
rsz <rhenansz@hotmail.com>
2022-07-24 07:40:30 -0300

    refactor: changed quotation marks on config.js

--- config/config.js
+++ config/config.js
@@ -11,7 +11,7 @@ module.exports = {
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: "database_test",
+    database: 'database_test',
     host: process.env.DB_HOST,
     dialect: 'mysql',
   },
